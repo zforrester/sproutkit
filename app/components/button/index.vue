4f8e54cdb8d994013822dcec066361e3493c77bb
Zach Forrester <zach.forrester@gmail.com>
2020-04-10 13:59:32 -0400

    Setup zero

---
+++ app/components/button/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <button type="button" name="button">My Button</button>
+</template>
+<script>
+module.exports = {
+    data: function() {
+        return {
+            greeting: 'Hello',
+        }
+    },
+}
+</script>
+<style scoped>
+button {
+    font-size: 2em;
+    text-align: center;
+}
+</style>
---
+++ app/components/comp.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Comps</h1>
+    <mybutton></mybutton>
+</template>
+<script>
+import mybutton from './button'
+module.exports = {
+    components: [mybutton],
+    data: function() {
+        return {
+            greeting: 'Hello',
+        }
+    },
+}
+</script>
+<style scoped>
+button {
+    font-size: 2em;
+    text-align: center;
+}
+</style>
