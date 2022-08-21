9933eda86b526ff716f108519f6c87a5d052a2e6
Zach Forrester <zach@Zachs-Work-MacBook-Pro.local>
2022-08-20 20:57:04 -0400

    setup formatting, stencil

---
+++ packages/component-library/stencil.config.ts
@@ -0,0 +1,30 @@
+import { Config } from '@stencil/core'
+
+import { sass } from '@stencil/sass'
+import { inlineSvg } from 'stencil-inline-svg'
+
+export const config: Config = {
+  namespace: 'component-library',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  plugins: [
+    sass({
+      // injectGlobalPaths: ['../../token-library/scss/main'],
+    }),
+    inlineSvg(),
+  ],
+}
